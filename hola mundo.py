a414e57aa12d2d6a15aef666817d260e0333edd2
Pablo <al_prodriguezf@iessierradelasvillas.org>
2022-09-29 10:01:39 +0200

    Clase de 29-9-2022

--- hola mundo.py
+++ hola mundo.py
@@ -1,1 +1,21 @@
-print("Hola mundo en Python")
+#Tipos de datos y variables.
+#numericos
+#enteros -- int
+#real -- float
+#Cadena de caracteres
+nombre = "Pablo"
+edad = 16 
+mayorOmenorEdad=False
+print(type(mayorOmenorEdad))
+print(type(nombre))
+print(type(edad))
+
+
+#Entrada y salida de datos.
+#Salida con print
+
+
+#Entrada de datos -- input ()
+nombre = input("Dime tu nombre:\n")
+edad = input("Dime tu edad:")
+print ("Buenos dias" , nombre, "tu edad es" , edad )
