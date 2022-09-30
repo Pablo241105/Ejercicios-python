6e355f2e1b39df09ce90dcf2ba84e3875cc8dc53
Pablo <al_prodriguezf@iessierradelasvillas.org>
2022-09-30 10:45:06 +0200

    Clase 30-09-2022

---
+++ Clase 30-09-2022.py
@@ -0,0 +1,19 @@
+#Sentencias condicionales
+
+if(5>4) :
+    print("Verdadero")
+else :
+   print("Falso")
+
+
+#Pimer ejercicio
+#Leer tu edad por teclado e imprima por pantalla si eres mayor de edad o menor
+edad=0
+edad = (int) (input("Dime tu edad: \n"))
+
+
+if(edad>=18) :
+    print("Eres mayor)")
+else :
+    print("Eres menor")
+    
