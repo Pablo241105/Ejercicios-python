4535df8aa7367385c1ffc9225dd4a50d7411ca07
Pablo241105 <al_prodriguezf@iessierradelasvillas.org>
2022-10-18 14:31:16 +0200

    Clase 18-10-2022

---
+++ Clase 18-10-2022.py
@@ -0,0 +1,3 @@
+def imprimirHola (nombre:str,apellido:str):
+    print("Hola",nombre,apellido)
+imprimirHola("juan","Zamora")
---
+++ Ejer 18-10-2022.py
@@ -0,0 +1,6 @@
+def sumaNumeros( numero1:int,numero2:int):
+    print(numero1+numero2)
+    return(numero1+numero2)
+
+print(sumaNumeros(2,2))
+print("La suma es:")
